72036a17633b4269a176ae00b22896f507ec7f2b
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-01-18 10:42:09 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,26 +1,12 @@
 let userId = null; // ユーザーIDを初期化
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    userId = profile.getId(); // ユーザーIDを取得
-
-    // チャットコンテナを表示
-    document.getElementById("chatContainer").style.display = "block";
-}
-
-function onFailure(error) {
-    console.log(error);
-    // ログイン失敗時の処理。必要に応じてここにコードを追加
-    // チャットコンテナを表示
-    document.getElementById("chatContainer").style.display = "block";
-}
-
 function sendMessage() {
     var message = document.getElementById("userInput").value;
     var postData = { message: message };
 
+    // userIdがある場合のみpostDataに追加
     if (userId !== null) {
-        postData.user_id = userId; // ユーザーIDがある場合のみ、送信データに追加
+        postData.user_id = userId;
     }
 
     fetch('/webhook', {
@@ -38,24 +24,7 @@ function sendMessage() {
     });
 }
 
-function renderButton() {
-    if (document.getElementById("loginButton")) {
-        gapi.signin2.render('loginButton', {
-            'scope': 'profile email',
-            'width': 240,
-            'height': 50,
-            'longtitle': true,
-            'theme': 'dark',
-            'onsuccess': onSignIn,
-            'onfailure': onFailure
-        });
-    } else {
-        // Googleログインボタンが存在しない場合は、チャットコンテナを直接表示
-        document.getElementById("chatContainer").style.display = "block";
-    }
-}
-
-// ページ読み込み時にGoogleログインボタンをレンダリング
+// ページ読み込み時にチャットコンテナを表示
 window.onload = function() {
-    renderButton();
+    document.getElementById("chatContainer").style.display = "block";
 };
