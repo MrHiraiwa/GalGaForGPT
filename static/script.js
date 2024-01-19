c8dab5087c781d8bc94e3714360acf3e3035642b
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-01-19 14:29:01 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -57,11 +57,11 @@ function sendMessage() {
             })
             .then(response => response.json())
             .then(data => {
+                playAudio(data.audio_url); // 音声を再生
                 var botMessageDiv = addBlankMessage(chatBox);
                 setUserMessage(botMessageDiv, data.reply, false, () => {
                     document.getElementById("userInput").disabled = false; // 入力ボックスを有効化
                     document.getElementById("userInput").focus();
-                    playAudio(data.audio_url); // 音声を再生
                 });
             });
             
