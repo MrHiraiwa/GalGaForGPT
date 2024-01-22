5fa6670a2be746db19625be06b19b7353d560b49
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-01-22 17:02:01 +0900

    Update functions_config.py

--- functions_config.py
+++ functions_config.py
@@ -1,30 +1,18 @@
 functions = [
     {
         # 関数の名称
-        "name": "search_music",
+        "name": "set_UserName",
         # 関数の機能説明
-        "description": "音楽を探す際に呼び出される。出力はすべて英語で行う",
+        "description": "You can set the name of the conversation partner.",
         # 関数のパラメータ
         "parameters": {
             "type": "object",
             # 各引数
             "properties": {
-                "genre": {
+                "username": {
                     "type": "string",
                     # 引数の説明
-                    "description": "探したい曲のジャンル"
-                },
-                "mood": {
-                    "type": "string",
-                    "description": "感情を表す言葉"
-                },
-                "artist": {
-                    "type": "string",
-                    "description": "探したいアーティスト名"
-                },
-                "title": {
-                    "type": "string",
-                    "description": "探したい曲のタイトル"
+                    "description": "ユーザーの名前"
                 }
             }
         }
