1cca11961f611a2e1efdabe0965363204c7c89c9
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-01-23 21:12:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import pytz
 import requests
 from datetime import datetime
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session
 from google.cloud import firestore
 from google.cloud import storage
 from google.auth.transport import requests as google_requests
