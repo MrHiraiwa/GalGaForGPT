0ce53241caf87fa0c27f2917662752df440cf684
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-01-23 18:10:42 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -77,6 +77,7 @@ def search_wikipedia(prompt):
 
 def scraping(links):
     contents = []
+    summary = ""
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36" ,
     }
