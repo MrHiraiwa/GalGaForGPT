e5ffba175d3671958a3b4694da6ba80a842e04ef
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-01-23 10:46:10 +0900

    Update functions_config.py

--- functions_config.py
+++ functions_config.py
@@ -22,5 +22,23 @@ functions = [
         "name": "clock",
         # 関数の機能説明
         "description": "useful for when you need to know what time it is.",
+    },
+        {
+        # 関数の名称
+        "name": "generate_image",
+        # 関数の機能説明
+        "description": "If the emotion or scene changes, be sure to specify the emotion or scene in one sentence and execute.",
+        # 関数のパラメータ
+        "parameters": {
+            "type": "object",
+            # 各引数
+            "properties": {
+                "prompt": {
+                    "type": "string",
+                    # 引数の説明
+                    "description": "画像生成プロンプト"
+                }
+            }
+        }
     }
 ]
