3a8966d7127c9874c9919e21b41f2afbca9403a3
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-01-25 01:06:53 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -65,7 +65,8 @@ function addMessageWithAnimation(chatBox, message, isUser) {
     var messageDiv = document.createElement('div');
     messageDiv.className = 'message-animation';
     
-    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])/g;
+    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])
+/g;
     let parts = message.split(urlRegex);
 
     parts.forEach(part => {
@@ -157,7 +158,8 @@ function sendMessage() {
 }
 
 function setUserMessage(messageDiv, message, isUser) {
-    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])/g;
+    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])
+/g;
     let fullMessage = message.split(urlRegex); // URLとその他のテキストを分割
 
     function createLinkElement(url) {
@@ -200,7 +202,8 @@ function setUserMessage(messageDiv, message, isUser) {
 
 
 function setBotMessage(messageDiv, message, isUser, callback) {
-    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])/g;
+    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])
+/g;
 
     function createLinkElement(url) {
         const link = document.createElement('a');
