a60ed4a044c2534ea722ca6f56122beaaeaaac9f
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-01-25 02:03:31 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -65,7 +65,7 @@ function addMessageWithAnimation(chatBox, message, isUser) {
     var messageDiv = document.createElement('div');
     messageDiv.className = 'message-animation';
     
-    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])/g;
+    const urlRegex = /https?:\/\/[^\s)]+[^)\s\p{Han}\p{Hiragana}\p{Katakana}]/gu;
 
     let parts = message.split(urlRegex);
 
@@ -158,7 +158,7 @@ function sendMessage() {
 }
 
 function setUserMessage(messageDiv, message, isUser) {
-    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])/g;
+    const urlRegex = /https?:\/\/[^\s)]+[^)\s\p{Han}\p{Hiragana}\p{Katakana}]/gu;
     let fullMessage = message.split(urlRegex); // URLとその他のテキストを分割
 
     function createLinkElement(url) {
@@ -201,7 +201,7 @@ function setUserMessage(messageDiv, message, isUser) {
 
 
 function setBotMessage(messageDiv, message, isUser, callback) {
-    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])/g;
+    const urlRegex = /https?:\/\/[^\s)]+[^)\s\p{Han}\p{Hiragana}\p{Katakana}]/gu;
 
     function createLinkElement(url) {
         const link = document.createElement('a');
