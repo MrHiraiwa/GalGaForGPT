63c15461ed254bec000d377762f4eb02947954e4
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-01-25 01:33:39 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -65,7 +65,7 @@ function addMessageWithAnimation(chatBox, message, isUser) {
     var messageDiv = document.createElement('div');
     messageDiv.className = 'message-animation';
     
-    const urlRegex = const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])
+    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])
 /g;
 
     let parts = message.split(urlRegex);
@@ -159,7 +159,7 @@ function sendMessage() {
 }
 
 function setUserMessage(messageDiv, message, isUser) {
-    const urlRegex = const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])
+    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])
 /g;
     let fullMessage = message.split(urlRegex); // URLとその他のテキストを分割
 
@@ -203,7 +203,7 @@ function setUserMessage(messageDiv, message, isUser) {
 
 
 function setBotMessage(messageDiv, message, isUser, callback) {
-    const urlRegex = const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])
+    const urlRegex = /(https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*[A-Za-z0-9-_~/?#])(?=\s|\Z|["'])
 /g;
 
     function createLinkElement(url) {
